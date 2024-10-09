0dd8e515d1475c0203531dc19c8e8dd17d14dfa5
Vithushan Logeswaran <lvithu123@outlook.com>
2024-10-09 14:11:58 +0530

    line 7 added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <h1> Hello </h1>
       <h2> Make this happening </h2>
       <h3> Adding comments in the Dev branch</h3>
+      <h4> Checking the CI/CD using Github actions</h4>
       
     </div>
   );
